e83e9e66833c9734e24b74fea8b8a912fc509ff2
Domenick Mifsud <47504507+domenick-m@users.noreply.github.com>
2022-08-21 00:02:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,62 @@
-//x-axis plot info
-let bottomTraces = [{ mode: "scatter" }];
-let bottomLayout = {
-  yaxis: { tickmode: "array", tickvals: [] },
-  xaxis: {
-    tickmode: "array",
-    tickvals: [0, 50, 100],
-    ticktext: ["0", "50", "100"],
-    range: [0, 100]
-  },
-  margin: { l: 100, t: 0 }
+// //x-axis plot info
+// let bottomTraces = [{ mode: "scatter" }];
+// let bottomLayout = {
+//   yaxis: { tickmode: "array", tickvals: [] },
+//   xaxis: {
+//     tickmode: "array",
+var trace1 = {
+  x: [1, 2, 3],
+  y: [4, 5, 6],
+  type: 'scatter'
 };
 
-//plot top/data plot
-Plotly.react(
-  "plot",
-  [{ x: [1, 10, 50, 60], y: [100, -100, 50, -30], mode: "scatter" }],
-  {
-    xaxis: {
-      range: [0, 100]
-    },
-    yaxis: {
-      zeroline: false
-    },
-    margin: { l: 100, b: 0 }
-  }
-);
+var trace2 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x2',
+  yaxis: 'y2',
+  type: 'scatter'
+};
 
-//plot x-axis
-Plotly.react("xaxis", bottomTraces, bottomLayout, { displayModeBar: false , responsive: true});
+var data = [trace1, trace2];
 
-//catch some top plot relayout events and redraw xaxis as an example
-document.getElementById("plot").on("plotly_relayout", function (data) {
-  if (data["xaxis.range[0]"]) {
-    bottomLayout.xaxis.range = [data["xaxis.range[0]"], data["xaxis.range[1]"]];
-  } else if (data["xaxis.autorange"]) {
-    bottomLayout.xaxis.range = [0, 100];
-  }
-  Plotly.react("xaxis", bottomTraces, bottomLayout, { displayModeBar: false , responsive: true});
-});
+var layout = {
+  grid: {rows: 1, columns: 2, pattern: 'independent'},
+};
+
+Plotly.newPlot('myDiv', data, layout);
+
+//     tickvals: [0, 50, 100],
+//     ticktext: ["0", "50", "100"],
+//     range: [0, 100]
+//   },
+//   margin: { l: 100, t: 0 }
+// };
+
+// //plot top/data plot
+// Plotly.react(
+//   "plot",
+//   [{ x: [1, 10, 50, 60], y: [100, -100, 50, -30], mode: "scatter" }],
+//   {
+//     xaxis: {
+//       range: [0, 100]
+//     },
+//     yaxis: {
+//       zeroline: false
+//     },
+//     margin: { l: 100, b: 0 }
+//   }
+// );
+
+// //plot x-axis
+// Plotly.react("xaxis", bottomTraces, bottomLayout, { displayModeBar: false , responsive: true});
+
+// //catch some top plot relayout events and redraw xaxis as an example
+// document.getElementById("plot").on("plotly_relayout", function (data) {
+//   if (data["xaxis.range[0]"]) {
+//     bottomLayout.xaxis.range = [data["xaxis.range[0]"], data["xaxis.range[1]"]];
+//   } else if (data["xaxis.autorange"]) {
+//     bottomLayout.xaxis.range = [0, 100];
+//   }
+//   Plotly.react("xaxis", bottomTraces, bottomLayout, { displayModeBar: false , responsive: true});
+// });
