59154aed7005be1cfb69c9cf8feb608ff7965d39
Domenick Mifsud <47504507+domenick-m@users.noreply.github.com>
2022-08-21 14:41:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,125 @@
-// //x-axis plot info
-// let bottomTraces = [{ mode: "scatter" }];
-// let bottomLayout = {
-//   yaxis: { tickmode: "array", tickvals: [] },
-//   xaxis: {
-//     tickmode: "array",
 var trace1 = {
-  x: [1, 2, 3],
   y: [4, 5, 6],
+  marker: {color: '#23b7e5'},
+  name: "Smoothed Spikes",
+  yaxis: 'y1',
+  type: 'scatter'
+};
+
+var trace1r = {
+  y: [5, 4, 4],
+  marker: {color: '#ff0000'},
+  name: "Rates",
+  yaxis: 'y1',
   type: 'scatter'
 };
 
 var trace2 = {
-  x: [20, 30, 40],
-  y: [50, 60, 70],
-  xaxis: 'x2',
+  y: [0.50, 0.60, 0.70],
+  marker: {color: '#23b7e5'},
+  showlegend: false,
   yaxis: 'y2',
+  // yaxis: 'y1',
+  // xaxis: 'x2',
+  type: 'scatter'
+};
+var trace2r = {
+  y: [0.6, 0.4, 0.4],
+  marker: {color: '#ff0000'},
+  showlegend: false,
+  yaxis: 'y2',
+  // yaxis: 'y1',
+  // xaxis: 'x2',
   type: 'scatter'
 };
 
-var data = [trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1, trace1];
-
-var layout = {
-  grid: {rows: 98, columns: 1, pattern: 'independent'},
+var trace3 = {
+  y: [50, 60, 70],
+  marker: {color: '#23b7e5'},
+  showlegend: false,
+  yaxis: 'y3',
+  // yaxis: 'y2',
+  type: 'scatter'
+};
+var trace3r = {
+  y: [40, 40, 80],
+  marker: {color: '#ff0000'},
+  showlegend: false,
+  yaxis: 'y3',
+  // yaxis: 'y2',
+  type: 'scatter'
 };
 
-var config = {responsive: true}
+var trace4 = {
+  y: [50, 60, 70],
+  marker: {color: '#23b7e5'},
+  showlegend: false,
+  yaxis: 'y4',
+  // yaxis: 'y2',
+  // xaxis: 'x2',
+  type: 'scatter'
+};
 
-Plotly.newPlot('myDiv', data, layout, config);
+var trace5 = {
+  y: [50, 60, 70],
+  marker: {color: '#23b7e5'},
+  showlegend: false,
+  yaxis: 'y5',
+  // yaxis: 'y3',
+  type: 'scatter'
+};
 
-//     tickvals: [0, 50, 100],
-//     ticktext: ["0", "50", "100"],
-//     range: [0, 100]
-//   },
-//   margin: { l: 100, t: 0 }
-// };
+var trace6 = {
+  y: [50, 60, 70],
+  marker: {color: '#23b7e5'},
+  showlegend: false,
+  yaxis: 'y6',
+  // yaxis: 'y3',
+  // xaxis: 'x2',
+  type: 'scatter'
+};
 
-// //plot top/data plot
-// Plotly.react(
-//   "plot",
-//   [{ x: [1, 10, 50, 60], y: [100, -100, 50, -30], mode: "scatter" }],
-//   {
-//     xaxis: {
-//       range: [0, 100]
-//     },
-//     yaxis: {
+var data = [trace1, trace1r, trace2, trace2r, trace3, trace3r, trace4, trace5, trace6];
+
+let bottomTraces = [{ mode: "scatter" }];
+let bottomLayout = {
+  yaxis: { tickmode: "array", tickvals: [] },
+  xaxis: {
+    tickmode: "array",
+    tickvals: [0, 50, 100],
+    ticktext: ["0", "50", "100"],
+    range: [0, 100],
+    domain: [0.05, 0.915]
+  },
+  margin: { l: 100, t: 0 },
+  
+};
+
+
+
+// var layout = {
+//   grid: {rows: 98, columns: 2}, yaxis: {
 //       zeroline: false
 //     },
 //     margin: { l: 100, b: 0 }
-//   }
-// );
+// };
 
-// //plot x-axis
-// Plotly.react("xaxis", bottomTraces, bottomLayout, { displayModeBar: false , responsive: true});
+var config = {responsive: true}
 
-// //catch some top plot relayout events and redraw xaxis as an example
-// document.getElementById("plot").on("plotly_relayout", function (data) {
-//   if (data["xaxis.range[0]"]) {
-//     bottomLayout.xaxis.range = [data["xaxis.range[0]"], data["xaxis.range[1]"]];
-//   } else if (data["xaxis.autorange"]) {
-//     bottomLayout.xaxis.range = [0, 100];
-//   }
-//   Plotly.react("xaxis", bottomTraces, bottomLayout, { displayModeBar: false , responsive: true});
-// });
+// Plotly.newPlot('myDiv', data, layout, config);
+Plotly.react(
+  "plot",
+  data,
+  {
+    xaxis: {
+      visible: false,
+    },
+    grid: {rows: 6, columns: 1},
+    yaxis: {
+      zeroline: true
+    },
+    margin: { l: 100, t: 25, b: 0 }
+  }
+);
+
+Plotly.react("xaxis", bottomTraces, bottomLayout, { displayModeBar: false });
